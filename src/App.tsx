95ad1797078921d5326f1fb08c1590f537f526e0
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-05-09 16:50:20 -0300

    creating task list

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState('')
+  const [taskList, setTaskList] = useState([])
+
+  const newTask = (event: any) => {
+    setTask(event.target.value)
+  }
+
+  const createNewTask = () => {
+    setTaskList([...taskList, task])
+    setTask('')
+  }
+
+  const removeTask = () => {
+    setTaskList([])
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <label>Adicione uma nova tarefa:</label>
+     <input onChange={newTask} type="text" c/>
+     <button onClick={createNewTask}>Criar +</button>
+     <button onClick={removeTask}>Remove</button>
+     {taskList.map((task) => (
+      <p>{task}</p>
+     ))}
     </>
   )
 }
