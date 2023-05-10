374e78cf06050ae6851ded1e7528bf0db64ede37
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-05-09 21:56:56 -0300

    section and header styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
-import './App.css'
+import styles from './App.module.css'
+import logoRocket from '../public/logo-rocket.svg'
+import Empty from '../public/empty.svg'
+import { PlusCircle } from 'phosphor-react'
 
 function App() {
   const [task, setTask] = useState('')
-  const [taskList, setTaskList] = useState([])
+  const [taskList, setTaskList] = useState([]) 
 
   const newTask = (event: any) => {
     setTask(event.target.value)
+    
   }
 
   const createNewTask = () => {
@@ -14,19 +18,48 @@ function App() {
     setTask('')
   }
 
-  const removeTask = () => {
-    setTaskList([])
+  const removeTask = (index: number) => {
+    const newTaskList = [...taskList]
+    newTaskList.splice(index, 1)
+    setTaskList(newTaskList)
   }
 
   return (
     <>
-     <label>Adicione uma nova tarefa:</label>
-     <input onChange={newTask} type="text" c/>
-     <button onClick={createNewTask}>Criar +</button>
-     <button onClick={removeTask}>Remove</button>
-     {taskList.map((task) => (
+    <header className={styles.header}>
+     <img src={logoRocket} alt="Foguete" />
+     <h1><span className={styles.to}>to</span><span className={styles.do}>do</span></h1>
+    </header>
+
+    <div className={styles.input}>
+     <input className={styles.inputAdd} onChange={newTask} type="text" placeholder='Adicione uma nova tarefa'/>
+     <button className={styles.buttonCreatePost} onClick={createNewTask}>Criar <PlusCircle size={20} /> </button>
+    </div>
+
+    <div className={styles.countTasks}>
+      <p className={styles.taskMade}>Tarefas criadas <span className={styles.spanCounterTask}>0</span></p>
+      <p className={styles.taskFinished}>Concluídas <span className={styles.spanCounterFinished}>0</span></p>
+    </div>
+
+    <div className={styles.line}>
+    <hr />
+    </div>
+
+    <div>
+    {taskList.length < 1 
+    ?
+    <div className={styles.nonTasks}>
+    <img className={styles.emptyImage} src={Empty} alt="Empty Image" />
+    <h3>Você ainda não tem tarefas cadastradas</h3> 
+    <h3 className={styles.h3opacity}>Crie tarefas e organize seus itens a fazer</h3>
+    </div>
+    :
+    <h1>oii</h1>
+    }
+    {taskList.map((task) => (
       <p>{task}</p>
      ))}
+     </div>
     </>
   )
 }
