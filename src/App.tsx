fa2192cd407779dc7dbd545f7a74e9577d5c5138
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-05-10 15:46:22 -0300

    tasks style

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,79 @@
-import { useState } from 'react'
-import styles from './App.module.css'
-import logoRocket from '../public/logo-rocket.svg'
-import Empty from '../public/empty.svg'
-import { PlusCircle } from 'phosphor-react'
+import { useState } from "react";
+import styles from "./App.module.css";
+import logoRocket from "../public/logo-rocket.svg";
+import Empty from "../public/empty.svg";
+import { PlusCircle, Trash } from "phosphor-react";
 
 function App() {
-  const [task, setTask] = useState('')
-  const [taskList, setTaskList] = useState([]) 
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
 
   const newTask = (event: any) => {
-    setTask(event.target.value)
-    
-  }
+    setTask(event.target.value);
+  };
 
   const createNewTask = () => {
-    setTaskList([...taskList, task])
-    setTask('')
-  }
+    setTaskList([...taskList, task]);
+    setTask("");
+  };
 
   const removeTask = (index: number) => {
-    const newTaskList = [...taskList]
-    newTaskList.splice(index, 1)
-    setTaskList(newTaskList)
-  }
+    const newTaskList = [...taskList];
+    newTaskList.splice(index, 1);
+    setTaskList(newTaskList);
+  };
 
   return (
     <>
-    <header className={styles.header}>
-     <img src={logoRocket} alt="Foguete" />
-     <h1><span className={styles.to}>to</span><span className={styles.do}>do</span></h1>
-    </header>
+      <header className={styles.header}>
+        <img src={logoRocket} alt="Foguete" />
+        <h1>
+          <span className={styles.to}>to</span>
+          <span className={styles.do}>do</span>
+        </h1>
+      </header>
 
-    <div className={styles.input}>
-     <input className={styles.inputAdd} onChange={newTask} type="text" placeholder='Adicione uma nova tarefa'/>
-     <button className={styles.buttonCreatePost} onClick={createNewTask}>Criar <PlusCircle size={20} /> </button>
-    </div>
+      <div className={styles.input}>
+        <input
+          className={styles.inputAdd}
+          onChange={newTask}
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+        />
+        <button className={styles.buttonCreatePost} onClick={createNewTask}>
+          Criar <PlusCircle size={20} />{" "}
+        </button>
+      </div>
 
-    <div className={styles.countTasks}>
-      <p className={styles.taskMade}>Tarefas criadas <span className={styles.spanCounterTask}>0</span></p>
-      <p className={styles.taskFinished}>Concluídas <span className={styles.spanCounterFinished}>0</span></p>
-    </div>
+      <div className={styles.countTasks}>
+        <p className={styles.taskMade}>
+          Tarefas criadas <span className={styles.spanCounterTask}>0</span>
+        </p>
+        <p className={styles.taskFinished}>
+          Concluídas <span className={styles.spanCounterFinished}>0</span>
+        </p>
+      </div>
 
-    <div className={styles.line}>
-    <hr />
-    </div>
+      <div className={styles.line}>
+        <hr />
+      </div>
 
-    <div>
-    {taskList.length < 1 
-    ?
-    <div className={styles.nonTasks}>
-    <img className={styles.emptyImage} src={Empty} alt="Empty Image" />
-    <h3>Você ainda não tem tarefas cadastradas</h3> 
-    <h3 className={styles.h3opacity}>Crie tarefas e organize seus itens a fazer</h3>
-    </div>
-    :
-    <h1>oii</h1>
-    }
-    {taskList.map((task) => (
-      <p>{task}</p>
-     ))}
-     </div>
+      <div>
+        {taskList.length === 0 ? (
+          <div className={styles.nonTasks}>
+            <img className={styles.emptyImage} src={Empty} alt="Empty Image" />
+            <h3>Você ainda não tem tarefas cadastradas</h3>
+            <h3 className={styles.h3opacity}>
+              Crie tarefas e organize seus itens a fazer
+            </h3>
+          </div>
+        ) : ( 
+        taskList.map((task) => (
+          <p className={styles.taskCreate}> <input className={styles.checked} type="radio" />  {task} <Trash size={18} className={styles.trashSvg} /></p>
+        )))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
